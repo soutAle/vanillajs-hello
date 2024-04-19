7463d4e24c8540339264e0f20ad375dadaa5d319
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-04-19 09:20:42 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function() {};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excusa(who, action, what, when) {
+  who = ["The dog", "My grandma", "The mailman", "My bird"];
+  action = ["ate", "peed", "crushed", "broke"];
+  what = ["my homework", "my phone", "the car"];
+  when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var randWho = Math.floor(Math.random() * who.length);
+  var randAction = Math.floor(Math.random() * action.length);
+  var randWhat = Math.floor(Math.random() * what.length);
+  var randWhen = Math.floor(Math.random() * when.length);
+
+  var excusa =
+    `${who[randWho]}` +
+    " " +
+    `${action[randAction]}` +
+    " " +
+    `${what[randWhat]}` +
+    " " +
+    `${when[randWhen]}`;
+  document.getElementById("excusa").innerHTML = excusa;
+  return excusa;
+}
+window.onload = excusa();
+excusa();
