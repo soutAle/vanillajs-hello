09daf38985286ea6b847c0a842c8d256332c67c3
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-04-24 21:33:55 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,34 +1,37 @@
-/* eslint-disable */
-
-window.onload = function() {};
-
-function excusa(who, action, what, when) {
-  who = ["The dog", "My grandma", "The mailman", "My bird"];
-  action = ["ate", "peed", "crushed", "broke"];
-  what = ["my homework", "my phone", "the car"];
-  when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
+window.onload = function () {
+  // Arrays con las partes de la excusa
+  var who = ["The dog", "My grandma", "The mailman", "My bird"];
+  var action = ["ate", "peed", "crushed", "broke"];
+  var what = ["my homework", "my phone", "the car"];
+  var when = [
+    "before the class!",
+    "when I was sleeping!",
+    "while I was exercising!",
+    "during my lunch!",
+    "while I was praying!"
   ];
 
-  var randWho = Math.floor(Math.random() * who.length);
-  var randAction = Math.floor(Math.random() * action.length);
-  var randWhat = Math.floor(Math.random() * what.length);
-  var randWhen = Math.floor(Math.random() * when.length);
+  // Función para generar una excusa aleatoria
+  function excusa(who, action, what, when) {
+    var randWho = Math.floor(Math.random() * who.length);
+    var randAction = Math.floor(Math.random() * action.length);
+    var randWhat = Math.floor(Math.random() * what.length);
+    var randWhen = Math.floor(Math.random() * when.length);
 
-  var excusa =
-    `${who[randWho]}` +
-    " " +
-    `${action[randAction]}` +
-    " " +
-    `${what[randWhat]}` +
-    " " +
-    `${when[randWhen]}`;
-  document.getElementById("excusa").innerHTML = excusa;
-  return excusa;
-}
-window.onload = excusa();
-excusa();
+    var excusa =
+      who[randWho] +
+      " " +
+      action[randAction] +
+      " " +
+      what[randWhat] +
+      " " +
+      when[randWhen];
+      
+    document.getElementById("Excusa").innerHTML = excusa;
+    document.getElementsByClassName("btn").innerHTML = excusa;
+    
+    return excusa;
+  }
+
+  excusa(who, action, what, when);
+};
